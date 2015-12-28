c4abc918274dd0a6691dd4a43d63cb3a0c25b49d
Jimmy Lv <jinglv@thoughtworks.com>
2015-12-28 21:33:39 +0800

    refactor MarsRover filed and split constructor parameters into 3 properties.

--- src/marsRover.js
+++ src/marsRover.js
@@ -1,10 +1,13 @@
-var MarsRover = function (initialPositionAlienation) {
-  this.initialPositionAlienation = initialPositionAlienation;
+var MarsRover = function (initialPositionAndOrientation) {
+  var result = initialPositionAndOrientation.split("").map(function(x){return parseInt(x)});
+  this.x = result[0];
+  this.y = result[1];
+  this.orientation = result[2];
 };
 
 MarsRover.prototype.execute = function (command) {
   if (command === "M") {
-    this.status = "0" + (parseInt(this.initialPositionAlienation.split("")[1])+1) + "N";
+    this.status = "0" + (this.y+1) + "N";
   } else if (command === "L") {
     this.status = "00W";
   } else if (command === "R") {

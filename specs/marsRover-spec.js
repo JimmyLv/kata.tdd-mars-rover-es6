1206447f577ae98a11e45a9bdd55f0006760b994
Jimmy Lv <jinglv@thoughtworks.com>
2015-12-28 21:25:08 +0800

    finish moveForward functionality.

--- specs/marsRover-spec.js
+++ specs/marsRover-spec.js
@@ -1,33 +1,31 @@
 describe("MarsRover", function () {
   var marsRover;
   beforeEach(function () {
-    var currentPosition = Position(0, 0);
-    marsRover = new MarsRover(currentPosition, "N");
+    marsRover = new MarsRover("00N");
   });
 
-  describe("#construction", function () {
-
-    it("should be a constructor function", function () {
-      expect(typeof MarsRover).toBe("function");
-    });
-
-    it("should be a constructor function taking two argument", function () {
-      expect(MarsRover.length).toBe(2);
-    });
-
-  });
-
-  describe("#moveForward", function () {
-
-    it("should return 00N given MarsRover with 01N received M command", function () {
-      marsRover.execute("M");
-      expect(marsRover.status).toBe("01N");
-    });
-
-    it("should return 00N given MarsRover with 00W received L command", function () {
+  describe("#received single command", function () {
+    it("should return 00W given MarsRover with 00N received L command", function () {
       marsRover.execute("L");
       expect(marsRover.status).toBe("00W");
     });
+
+    it("should return 00E given MarsRover with 00N received R command", function () {
+      marsRover.execute("R");
+      expect(marsRover.status).toBe("00E");
+    });
+
+
+    it("should return 01N given MarsRover with 00N received M command", function () {
+      marsRover.execute("M");
+      expect(marsRover.status).toBe("01N");
+    });
+    //
+    //it("should return 01W given MarsRover with 11W received M command", function () {
+    //  var marsRover2 = new MarsRover("11W");
+    //  marsRover2.execute("M");
+    //  expect(marsRover2.status).toBe("01W");
+    //});
 
   });
 });
--- src/marsRover.js
+++ src/marsRover.js
@@ -1,12 +1,13 @@
-var MarsRover = function (position, orientation) {
-  this.position = position;
-  this.orientation = orientation;
+var MarsRover = function (initialPositionAlienation) {
+  this.initialPositionAlienation = initialPositionAlienation;
 };
 
 MarsRover.prototype.execute = function (command) {
   if (command === "M") {
-    this.status = "01N";
+    this.status = "0" + (parseInt(this.initialPositionAlienation.split("")[1])+1) + "N";
   } else if (command === "L") {
     this.status = "00W";
+  } else if (command === "R") {
+    this.status = "00E";
   }
 };
